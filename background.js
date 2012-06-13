21ba9ead4134a2857f8ce8c3a2ce8b6114ef0308
Drake Christensen <drakechristensen@gmail.com>
2012-06-13 13:14:50 -0500

    updated README and comments

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-// TODO:
-// - Also remove HTML5 local storage
-// - Reload current tab
-// - publish on github
-// - is having this enabled screwing up the Ads Block Plus extension?
-
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
   // If the letter 'g' is found in the tab's URL...
